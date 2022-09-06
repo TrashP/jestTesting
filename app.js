a1e70df9a66e7a49b21c07969ff0d10206c24bfb
Arnab <arnabdnri@gmail.com>
2022-09-06 12:15:56 +0530

    Add caesarCipher and analyzeArray functions for testing

--- app.js
+++ app.js
@@ -6,4 +6,53 @@ function reverseString(str) {
 	return str.split('').reverse().join('');
 }
 
-export { capitalize, reverseString };
+const Calculator = {
+	add: function (a, b) {
+		return a + b;
+	},
+
+	subtract: function (a, b) {
+		return a - b;
+	},
+
+	multiply: function (a, b) {
+		return a * b;
+	},
+
+	divide: function (a, b) {
+		return a / b;
+	},
+};
+
+function caesarCipher(str) {
+	let result = '';
+	let charCode = 0;
+
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] == ' ') {
+			result += ' ';
+		} else if (str[i] == 'z') {
+			result += 'a';
+		} else if (str[i] == 'Z') {
+			result += 'A';
+		} else {
+			charCode = str[i].charCodeAt() + 1;
+			result += String.fromCharCode(charCode);
+		}
+	}
+
+	return result;
+}
+
+function analyzeArray(arr) {
+	return {
+		average: arr.reduce((a, b) => a + b) / arr.length,
+		min: Math.min(...arr),
+		max: Math.max(...arr),
+		length: arr.length,
+	};
+}
+
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]));
+
+export { capitalize, reverseString, Calculator, caesarCipher, analyzeArray };
--- app.test.js
+++ app.test.js
@@ -1,4 +1,10 @@
-import { capitalize, reverseString } from './app.js';
+import {
+	capitalize,
+	reverseString,
+	Calculator,
+	caesarCipher,
+	analyzeArray,
+} from './app.js';
 
 // capitalize strings
 test('capitalize "hello" to "Hello"', () => {
@@ -16,4 +22,38 @@ test('reverse "archipelago" to "ogalepihcra"', () => {
 
 test('reverse "to be or not to be" to "eb ot ton ro eb ot"', () => {
 	expect(reverseString('to be or not to be')).toBe('eb ot ton ro eb ot');
+});
+
+//calculator
+test('add 2 + 3 to give 5', () => {
+	expect(Calculator.add(2, 3)).toBe(5);
+});
+
+test('subtract 9 - 7 to give 2', () => {
+	expect(Calculator.subtract(9, 7)).toBe(2);
+});
+
+test('multiply 4 * 5 to give 20', () => {
+	expect(Calculator.multiply(4, 5)).toBe(20);
+});
+
+test('divide 30 / 6 to give 5', () => {
+	expect(Calculator.divide(30, 6)).toBe(5);
+});
+
+//caesarCipher
+test('encrypt "Attack the Enemy; in the haze" to "Buubdl uif Fofnz< jo uif ibaf"', () => {
+	expect(caesarCipher('Attack the Enemy; in the haze')).toBe(
+		'Buubdl uif Fofnz< jo uif ibaf'
+	);
+});
+
+//analyzeArray
+test('analyze [1, 8, 3, 4, 2, 6] as { average: 4, min: 1, max: 8, length: 6 }', () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+		average: 4,
+		min: 1,
+		max: 8,
+		length: 6,
+	});
 });
